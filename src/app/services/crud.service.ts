5d2c64ce559ada88be61a3d96ae1b948af40c9cd
supunjh <63024774+supunjh@users.noreply.github.com>
2022-03-23 20:42:37 +0530

    firebase work

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -8,7 +8,7 @@ export class CrudService {
 
   constructor(public fireservices:AngularFirestore) { }
 create_newuser(Record: any){
-
+  console.log("RECORD ",Record)
   return this.fireservices.collection('user').add(Record);
 
 }
